83953a502049a7c7c3b68a37cbdda68fb8fe9718
1604197 <100869144+1604197@users.noreply.github.com>
2025-06-16 13:02:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config(); 
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+//GET -> keep server on render alive
+app.get('/upload', async (request, response) =>{
+    response.status(200).send(process.env.LAMBDA_UP)
+})
+
+app.get('/upload-large', async (request, response) =>{
+    response.status(200).send(process.env.LAMBDA_UP_LARGE)
+})
+
+app.get('/download', async (request, response) =>{
+    response.status(200).send(process.env.LAMBDA_DOWN)
+})
+
+const PORT = 3001
+app.listen(PORT, () =>{ 
+    console.log('Server running on port ' + PORT)
+})
+
