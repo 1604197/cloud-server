0aad511d287ff15a1c34afd451bcd72939fa70b5
1604197 <100869144+1604197@users.noreply.github.com>
2025-06-16 13:24:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/download', async (request, response) =>{
 })
 
 const PORT = 3001
-app.listen(PORT, ('0.0.0.0') =>{ 
+app.listen(PORT, '0.0.0.0' () =>{ 
     console.log('Server running on port ' + PORT)
 })
 
